175dcc8ace002d86ef7e1c7a9d3ce2ad21d3e893
A. Narendra Jhabakh <njhabakh@gmail.com>
2024-06-07 14:30:29 -0400

    Updated UI and usecase

--- app.py
+++ app.py
@@ -24,14 +24,22 @@ from langchain.vectorstores import FAISS
 from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 
+from pypdf import PdfReader
+
 ## Bedrock Clients
 bedrock=boto3.client(service_name="bedrock-runtime")
 bedrock_embeddings=BedrockEmbeddings(model_id="amazon.titan-embed-text-v1",client=bedrock)
 
 
+#Extract PDF Data
+def extract_pdf(filename):
+    reader = PdfReader(filename)
+    page = reader.pages[0]
+    return page.extract_text()
+
 ## Data ingestion
-def data_ingestion():
-    loader=PyPDFDirectoryLoader("reports")
+def data_ingestion(inp):
+    loader=PyPDFDirectoryLoader(inp)
     documents=loader.load()
 
     # - in our testing Character split works better with this PDF data set
@@ -43,17 +51,17 @@ def data_ingestion():
 
 ## Vector Embedding and vector store
 
-def get_vector_store(docs):
+def get_vector_store(docs, inp):
     vectorstore_faiss=FAISS.from_documents(
         docs,
         bedrock_embeddings
     )
-    vectorstore_faiss.save_local("faiss_index_reports")
+    vectorstore_faiss.save_local(f"faiss_index_{inp}")
 
 def get_claude_llm():
     ##create the Anthropic Model
     llm=BedrockChat(model_id="anthropic.claude-3-sonnet-20240229-v1:0",client=bedrock,
-                model_kwargs={'max_tokens':200})
+                model_kwargs={'max_tokens':1000})
     
     return llm
 
@@ -64,7 +72,8 @@ def get_llama2_llm():
     
     return llm
 
-prompt_template = """
+
+prompt_template_chat = """
 
 Human: Use the following pieces of context to provide a 
 concise answer to the question at the end but usse atleast summarize with 
@@ -78,11 +87,33 @@ Question: {question}
 
 Assistant:"""
 
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context", "question"]
+
+prompt_template_compliance = """
+Imagine you are a compliance officer for a bank checking if policies and guidelines are being met.
+Check the sections of the following document on whether the policies are being met.
+<question>
+{question}
+</question
+
+The following are the poilicies to be checked against:
+<context>
+{context}
+</context
+
+Provide the reason for non compliance with the corresponding section of the document 
+and suggest edits to be made. Be as granular as possible.
+
+Assistant:"""
+
+PROMPT1 = PromptTemplate(
+    template=prompt_template_compliance, input_variables=["context", "question"]
 )
 
-def get_response_llm(llm,vectorstore_faiss,query):
+PROMPT2 = PromptTemplate(
+    template=prompt_template_chat, input_variables=["context", "question"]
+)
+
+def get_response_llm(llm,vectorstore_faiss,query, PROMPT):
     qa = RetrievalQA.from_chain_type(
     llm=llm,
     chain_type="stuff",
@@ -97,37 +128,43 @@ def get_response_llm(llm,vectorstore_faiss,query):
 
 
 def main():
-    st.set_page_config("Chat PDF")
-    
-    st.header("Chat with PDF using AWS Bedrock💁")
+    st.set_page_config("Team LLM")
 
-    user_question = st.text_input("Ask a Question from the PDF Files")
-
-    with st.sidebar:
-        st.title("Update Or Create Vector Store:")
+    uploaded_file = st.file_uploader('Choose your .pdf file', type="pdf")
+    tab1, tab2, tab3 = st.tabs(["Compi-Ease", "Compliance-Bot", "Regu-sinc"])
+    with tab1:
+        # user_question = st.text_input("Ask a Question from the PDF Files")
         
-        if st.button("Vectors Update"):
+        if uploaded_file is not None:
+            user_question = extract_pdf(uploaded_file)
+
+        with st.sidebar:
+            st.title("Update Or Create Vector Store:")
+            
+            if st.button("Vectors Update Guidelines"):
+                with st.spinner("Processing..."):
+                    docs = data_ingestion('guidelines')
+                    get_vector_store(docs, 'guidelines')
+                    st.success("Done")
+
+        if st.button("Claude Output"):
             with st.spinner("Processing..."):
-                docs = data_ingestion()
-                get_vector_store(docs)
+                faiss_index = FAISS.load_local("faiss_index_guidelines", bedrock_embeddings, allow_dangerous_deserialization=True)
+                llm=get_claude_llm()
+                
+                st.write(get_response_llm(llm,faiss_index,user_question, PROMPT1))
                 st.success("Done")
 
-    if st.button("Claude Output"):
-        with st.spinner("Processing..."):
-            faiss_index = FAISS.load_local("faiss_index_reports", bedrock_embeddings, allow_dangerous_deserialization=True)
-            llm=get_claude_llm()
-            
-            #faiss_index = get_vector_store(docs)
-            st.write(get_response_llm(llm,faiss_index,user_question))
-            st.success("Done")
+    with tab2:
+        st.header("Compliance-Bot")
 
-    if st.button("Llama2 Output"):
-        with st.spinner("Processing..."):
-            faiss_index = FAISS.load_local("faiss_index_reports", bedrock_embeddings, allow_dangerous_deserialization=True)
-            llm=get_llama2_llm()
-            
-            st.write(get_response_llm(llm,faiss_index,user_question))
-            st.success("Done")
+
+    with tab3:
+        st.header("Regu-sinc")
+        st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
+
+    
+
 
 if __name__ == "__main__":
     main()
